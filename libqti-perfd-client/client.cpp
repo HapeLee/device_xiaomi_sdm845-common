47703cb183a15508c3713dfedc99fd3302a94fc8
Chenyang Zhong <zhongcy95@gmail.com>
2020-08-13 05:49:26 -0400

    sdm845-common: fake perf lock acquire and release
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- libqti-perfd-client/client.cpp
+++ libqti-perfd-client/client.cpp
@@ -3,8 +3,8 @@
 namespace android {
     extern "C" void perf_get_feedback() {}
     extern "C" void perf_hint() {}
-    extern "C" void perf_lock_acq() {}
+    extern "C" int perf_lock_acq(int handle, int duration, int list[], int numArgs) { return (handle > 0) ? handle : 1; }
     extern "C" void perf_lock_cmd() {}
-    extern "C" void perf_lock_rel() {}
+    extern "C" int perf_lock_rel(int handle) { return 0; }
     extern "C" void perf_lock_use_profile() {}
 }
